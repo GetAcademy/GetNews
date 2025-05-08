682025c2ab65108d752beed0bfb8984b23535590
Kristoffer <krigjo25@gmail.com>
2025-05-08 09:26:27 +0200

    IC

--- GetNews.Core/ApplicationService/SubscriptionService.cs
+++ GetNews.Core/ApplicationService/SubscriptionService.cs
@@ -1,4 +1,5 @@
 ﻿//  SIgnup process for a subscription
+
 using System.Net.Mail;
 using GetNews.Core.DomainModel;
 
@@ -7,25 +8,22 @@ namespace GetNews.Core.ApplicationService
     public class SubscriptionService
     {
 
+        /// <summary>
+        /// When a user signs up for a subscription, the system will check if the email address is valid.
+        /// If the email address is valid, the system will check if the user is already subscribed.
+        /// If the user is not already subscribed, the system will create a new subscription object and send a confirmation email to the user.
+        /// </summary>
+        /// <param name="emailAddressStr">The email address of the user</param>
+        /// <param name="subscription">The subscription object of the user</param>
+        /// <returns>A SignUpResult object containing the result of the sign-up process</returns>
+
         public static SignUpResult SignUp(string emailAddressStr, Subscription? subscription)
         {
-            /*
-                *   When a user signs up for a subscription, the system will check if the email address is valid.
-                *   If the email address is valid, the system will check if the user is already subscribed.
-
-                *   @param emailAddressStr: The email address of the user
-                *   @param subscription: The subscription object of the user
-                *   @return: A SignUpResult object containing the result of the sign-up process
-            */
-
-            //  Ensure the email address is not registered
-
-
             var emailAddress = new EmailAddress(emailAddressStr);
             if (!emailAddress.IsValid())
                 return SignUpResult.Fail(SignUpError.InvalidEmailAddress);
 
-            // Hvis ingen subscription finnes, opprett ny
+            // create a new subscription object.
             if (subscription == null)
             {
                 subscription = new Subscription(emailAddressStr);
