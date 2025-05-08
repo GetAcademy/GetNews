ea3319183ead9fff9dca1c36a10a564bac536c91
GET Academy <terje@kolderup.net>
2025-05-08 12:54:30 +0200

    x

---
+++ GetNews.Core/DomainModel/EmailAndSubscription.cs
@@ -0,0 +1,14 @@
+﻿namespace GetNews.Core.DomainModel
+{
+    internal class EmailAndSubscription
+    {
+        public Email Email { get;  }
+        public Subscription Subscription { get;  }
+
+        public EmailAndSubscription(Email email, Subscription subscription)
+        {
+            Email = email;
+            Subscription = subscription;
+        }
+    }
+}
---
+++ GetNews.Core/DomainModel/Result.cs
@@ -0,0 +1,21 @@
+﻿namespace GetNews.Core.DomainModel
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public T Value { get; }
+        public string Error { get; }
+
+        protected Result(T value, bool isSuccess, string error)
+        {
+            Value = value;
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result<T> Success(T value) => new Result<T>(value, true, null);
+        public static Result<T> Failure(string error) => new Result<T>(default, false, error);
+        public static Result<T> Failure<TENUM>(TENUM error) => new Result<T>(default, false, error.ToString());
+    }
+
+}
